aae542bf51e1738cf0fb1b67b889d76db4eaa28e
Daniel Niehoff <d.niehoff86@googlemail.com>
2013-07-17 23:21:21 +0200

    #6

--- MakeMyReport/MakeMyReport/src/main/java/com/project/makemyreport/Activity_EditReport.java
+++ MakeMyReport/MakeMyReport/src/main/java/com/project/makemyreport/Activity_EditReport.java
@@ -3,7 +3,6 @@ package com.project.makemyreport;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.TextView;
 
 /**
  * Created by Vitali on 15.07.13.
@@ -16,10 +15,6 @@ public class Activity_EditReport extends Activity {
 
         // -1 = Neuer Report ansonsten interne ID des Reports
         int ReportID = getIntent().getIntExtra("ReportID", -1);
-
-        TextView reportid = (TextView) findViewById(R.id.editreport_reportid);
-        reportid.setText("Report_ID" + ReportID);
-
 
     }
 }
