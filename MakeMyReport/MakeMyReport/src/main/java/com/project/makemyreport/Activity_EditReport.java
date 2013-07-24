8ac9f2a25bb8f35e30e64e852828d1aa8d0c09f7
Daniel Niehoff <d.niehoff86@googlemail.com>
2013-07-24 15:26:23 +0200

    #6

--- MakeMyReport/MakeMyReport/src/main/java/com/project/makemyreport/Activity_EditReport.java
+++ MakeMyReport/MakeMyReport/src/main/java/com/project/makemyreport/Activity_EditReport.java
@@ -1,13 +1,20 @@
 package com.project.makemyreport;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Window;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Vitali on 15.07.13.
  */
 public class Activity_EditReport extends Activity {
+
+    ImageView mIssuePicture;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -16,5 +23,28 @@ public class Activity_EditReport extends Activity {
         // -1 = Neuer Report ansonsten interne ID des Reports
         int ReportID = getIntent().getIntExtra("ReportID", -1);
 
+        InitIssue();
+
     }
+
+    private void InitIssue(){
+
+        mIssuePicture = (ImageView) findViewById(R.id.issuePicture);
+
+        mIssuePicture.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent(1);
+            }
+
+        });
+
+    }
+
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, actionCode);
+    }
+
 }
